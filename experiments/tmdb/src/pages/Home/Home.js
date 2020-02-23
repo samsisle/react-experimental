799d9167418ec5ab45fe38005d9d42e5a4f70f7b
samsisle <samko9522@gmail.com>
2020-02-23 00:45:29 -0800

    Remove dynamic padding <div>

--- experiments/tmdb/src/pages/Home/Home.js
+++ experiments/tmdb/src/pages/Home/Home.js
@@ -8,20 +8,17 @@ import css from './css/Home.module.css';
 
 export default function Home() {
   return (
-    <>
-      <div className={css.dynamicPaddingTop}></div>
-      <main className={css.container}>
-        <h2>Now Playing</h2>
-        <div className="description">Movies now playing in theaters.</div>
-        <Suspense fallback={<PostersPlaceholder />}>
-          <NowPlaying />
-        </Suspense>
-        <h2>Upcoming</h2>
-        <div className="description">Movies coming soon to theaters.</div>
-        <Suspense fallback={<PostersPlaceholder />}>
-          <Upcoming />
-        </Suspense>
-      </main>
-    </>
+    <main className={css.container}>
+      <h2>Now Playing</h2>
+      <div className="description">Movies now playing in theaters.</div>
+      <Suspense fallback={<PostersPlaceholder />}>
+        <NowPlaying />
+      </Suspense>
+      <h2>Upcoming</h2>
+      <div className="description">Movies coming soon to theaters.</div>
+      <Suspense fallback={<PostersPlaceholder />}>
+        <Upcoming />
+      </Suspense>
+    </main>
   );
 }
