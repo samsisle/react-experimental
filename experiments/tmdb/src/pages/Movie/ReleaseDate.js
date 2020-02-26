fb44a2b0087f06ca4d886ab5e5dc3dd88fc24fb1
samsisle <samko9522@gmail.com>
2020-02-25 19:02:24 -0800

    Update formatTimestamp function

--- experiments/tmdb/src/pages/Movie/ReleaseDate.js
+++ experiments/tmdb/src/pages/Movie/ReleaseDate.js
@@ -8,8 +8,8 @@ export default function ReleaseDate(props) {
   function formatTimestamp(releaseDate) {
     const t = releaseDate.slice(0, 10);
 
-    const day = convertToOrdinal(t.slice(-2));
     const month = months[Number(t.slice(5, 7)) - 1];
+    const day = convertToOrdinal(t.slice(-2));
     const year = t.slice(0, 4);
 
     return `${month} ${day}, ${year}`;
