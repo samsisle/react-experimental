0784332cb7983f8d4323a608f9d111609548b93d
samsisle <samko9522@gmail.com>
2020-02-25 18:37:00 -0800

    Rename releaseDateConversion to formatTimestamp

--- experiments/tmdb/src/pages/Movie/ReleaseDate.js
+++ experiments/tmdb/src/pages/Movie/ReleaseDate.js
@@ -5,12 +5,12 @@ import { months, convertToOrdinal } from '../../lib/calendar';
 import css from './css/Info.module.css';
 
 export default function ReleaseDate(props) {
-  function releaseDateConversion(releaseDate) {
+  function formatTimestamp(releaseDate) {
     const t = releaseDate.slice(0, 10);
 
-    const year = t.slice(0, 4);
-    const month = months[Number(t.slice(5, 7)) - 1];
     const day = convertToOrdinal(t.slice(-2));
+    const month = months[Number(t.slice(5, 7)) - 1];
+    const year = t.slice(0, 4);
 
     return `${month} ${day}, ${year}`;
   }
@@ -18,7 +18,7 @@ export default function ReleaseDate(props) {
   return (
     <li className={css.info}>
       <span className={css.credits}>Release</span>
-      {releaseDateConversion(props.releaseDate)}
+      {formatTimestamp(props.releaseDate)}
     </li>
   );
 }
