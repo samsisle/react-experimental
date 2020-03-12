83b3721479e273a8d179b2262fecc40cb15e0f15
samsisle <samko9522@gmail.com>
2020-03-11 22:47:30 -0700

    Update Search component + styles

--- experiments/tmdb/src/components/Search/Search.js
+++ experiments/tmdb/src/components/Search/Search.js
@@ -9,13 +9,15 @@ import css from './Search.module.css';
 export default function Search() {
   const [toggle, set] = useState(false);
   const [search, setSearch] = useState('');
-  const containerRef = useRef(null);
+  const searchRef = useRef(null);
 
   const expandClassName = `${toggle ? ' ' : ''}${toggle ? css.expand : ''}`;
+  const borderClassName = `${toggle ? ' ' : ''}${toggle ? css.border : ''}`;
+  const visibleClassName = `${toggle ? ' ' : ''}${toggle ? css.visible : ''}`;
 
   useEffect(() => {
     function handleClickOutside(e) {
-      if (containerRef.current && containerRef.current.contains(e.target)) {
+      if (searchRef.current && searchRef.current.contains(e.target)) {
         set(true);
       } else {
         set(false);
@@ -27,32 +29,43 @@ export default function Search() {
       // Unbind the event listener on clean up
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [containerRef]);
+  }, [searchRef]);
+
+  useEffect(() => {
+    async function fetchSearch(query) {
+      const encodedQuery = query.replace(' ', '%20');
+      const data = await fetch(
+        `https://api.themoviedb.org/3/search/movie?language=en-US&query=${encodedQuery}&page=1&include_adult=false&region=US`
+      );
+    }
+
+    if (search.length > 0) {
+      fetchSearch(search);
+    }
+  }, [search]);
 
   function handleSearchChange(e) {
     setSearch(e.target.value);
   }
 
-  // async function fetchSearch(query) {
-  //   const encodedQuery = query.replace(' ', '%20');
-  //   const data = await fetch(
-  //     `https://api.themoviedb.org/3/search/movie?language=en-US&query=${encodedQuery}&page=1&include_adult=false&region=US`
-  //   );
-  // }
-
-  // fetchSearch(search);
-
   return (
-    <div className={`${css.container}${expandClassName}`} ref={containerRef}>
-      <div className={css.icon}>
-        <SearchIcon />
+    <div className={`${css.container}${expandClassName}`}>
+      <div className={`${css.search}${borderClassName}`} ref={searchRef}>
+        <div className={css.icon}>
+          <SearchIcon />
+        </div>
+        <input
+          className={css.input}
+          onChange={handleSearchChange}
+          placeholder="Search TMDb..."
+          value={search}
+        />
       </div>
-      <input
-        className={css.input}
-        onChange={handleSearchChange}
-        placeholder="Search TMDb..."
-        value={search}
-      />
+      <div className={`${css.results}${visibleClassName}`}>
+        <ul>
+          <li>bruh!</li>
+        </ul>
+      </div>
     </div>
   );
 }
