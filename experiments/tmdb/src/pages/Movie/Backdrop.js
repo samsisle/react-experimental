28d4636990153540b16e48379c860f5193a09a61
samsisle <samko9522@gmail.com>
2020-03-11 19:23:45 -0700

    Update Backdrop component + styles

--- experiments/tmdb/src/pages/Movie/Backdrop.js
+++ experiments/tmdb/src/pages/Movie/Backdrop.js
@@ -9,13 +9,11 @@ export default function Backdrop() {
 
   return (
     <section className={css.backdrop}>
-      {movie.backdrop_path ? (
+      {movie.backdrop_path && (
         <img
           src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
           alt={movie.title}
         />
-      ) : (
-        <div className={css.placeholder}></div>
       )}
     </section>
   );
