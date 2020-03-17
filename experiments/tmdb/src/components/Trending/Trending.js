80aaa7fc8abd708c4cc6886c92bfe550e1798706
samsisle <samko9522@gmail.com>
2020-03-16 19:13:55 -0700

    Update Trending component + styles

--- experiments/tmdb/src/components/Trending/Trending.js
+++ experiments/tmdb/src/components/Trending/Trending.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useSwr from 'swr';
+
+import formatTitle from '../../lib/formatTitle';
 
 import css from './Trending.module.css';
 
-export default function Trending() {
+export default function Trending(props) {
+  const navigate = useNavigate();
+
   const { data: trending } = useSwr(
     'https://api.themoviedb.org/3/trending/movie/day',
     {
@@ -11,11 +16,26 @@ export default function Trending() {
     }
   );
 
+  function navigateToMoviePage(id, title) {
+    return () => {
+      props.set(false);
+      props.setSearch('');
+      props.setResults([]);
+      navigate(`/${id}/${formatTitle(title)}`);
+    };
+  }
+
   return (
     <>
       <div className={css.trending}>Trending</div>
       {trending.results.slice(0, 7).map(result => (
-        <li>{result.title}</li>
+        <li
+          key={result.id}
+          className={css.result}
+          onClick={navigateToMoviePage(result.id, result.title)}
+        >
+          <div className={css.title}>{result.title}</div>
+        </li>
       ))}
     </>
   );
